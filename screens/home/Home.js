35ead6c4bbf93ce66add28c03d74d31f6c6c6932
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-02-15 12:37:57 -0300

    Estilização home

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,19 +1,79 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, TouchableOpacity, Image, View } from 'react-native';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text>Home</Text>
-    </View>
+
+      <View style={styles.ContRow}>
+        <TouchableOpacity style={styles.cardDefesa}>
+          <Image style={styles.icons} source={require('../../assets/images/selfDefense.png')} />
+          <Text>Defesa pessoal</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.cardDefesa}>
+          <Image style={styles.icons} source={require('../../assets/images/selfDefense.png')} />
+          <Text>Defesa pessoal</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.ContRow}>
+        <TouchableOpacity style={styles.cardDefesa}>
+          <Image style={styles.icons} source={require('../../assets/images/selfDefense.png')} />
+          <Text>Defesa pessoal</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.cardDefesa}>
+          <Image style={styles.icons} source={require('../../assets/images/selfDefense.png')} />
+          <Text>Defesa pessoal</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.ContRow}>
+        <TouchableOpacity style={styles.cardDefesa}>
+          <Image style={styles.icons} source={require('../../assets/images/selfDefense.png')} />
+          <Text>Defesa pessoal</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.cardDefesa}>
+          <Image style={styles.icons} source={require('../../assets/images/selfDefense.png')} />
+          <Text>Defesa pessoal</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.ContRow}>
+        <TouchableOpacity style={styles.cardDefesa}>
+          <Image style={styles.icons} source={require('../../assets/images/selfDefense.png')} />
+          <Text>Defesa pessoal</Text>
+        </TouchableOpacity>
+      </View>
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      flexDirection: 'column',
       backgroundColor: '#fff',
+    },
+    ContRow: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+    },
+    cardDefesa: {
+      marginTop: 20,
+      backgroundColor: '#400040',
+      width: 150,
+      height: 150,
+      borderRadius: 15,
       alignItems: 'center',
-      justifyContent: 'center',
+      justifyContent: 'space-around',
+    },
+    icons: {
+      width: 70,
+      height: 70,
     },
   });
