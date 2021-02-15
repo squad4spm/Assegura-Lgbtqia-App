c2e1caa9a8f0b85d3b932516dd164a81d12cff8f
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-02-15 11:26:55 -0300

    Rotas app

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+import * as Screens from "./screens";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Assegura!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Screens.} />
+        <Drawer.Screen name="Defesa pessoal" component={Screens.} />
+        <Drawer.Screen name="Nome social" component={Screens.} />
+        <Drawer.Screen name="Localizações" component={Screens.} />
+        <Drawer.Screen name="Ajude" component={Screens.} />
+        <Drawer.Screen name="Casas de acolhimento" component={Screens.} />
+        <Drawer.Screen name="Direitos LGBTQIA+" component={Screens.} />
+        <Drawer.Screen name="Ajuda psicológica" component={Screens.} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
