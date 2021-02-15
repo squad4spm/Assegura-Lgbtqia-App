f6c0b3035d895ccf81b99e14ed405988d9322a6f
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-02-15 11:36:38 -0300

    Rotas

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
-import * as Screens from "./screens";
+import * as Screens from './screens';
 
 const Drawer = createDrawerNavigator();
 
@@ -11,14 +11,14 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Screens.} />
-        <Drawer.Screen name="Defesa pessoal" component={Screens.} />
-        <Drawer.Screen name="Nome social" component={Screens.} />
-        <Drawer.Screen name="Localizações" component={Screens.} />
-        <Drawer.Screen name="Ajude" component={Screens.} />
-        <Drawer.Screen name="Casas de acolhimento" component={Screens.} />
-        <Drawer.Screen name="Direitos LGBTQIA+" component={Screens.} />
-        <Drawer.Screen name="Ajuda psicológica" component={Screens.} />
+        <Drawer.Screen name="Home" component={Screens.Home} />
+        <Drawer.Screen name="Defesa pessoal" component={Screens.Defesa_pessoal} />
+        <Drawer.Screen name="Nome social" component={Screens.Nome_social} />
+        <Drawer.Screen name="Localizações" component={Screens.Localizacoes} />
+        <Drawer.Screen name="Ajude" component={Screens.Ajude} />
+        <Drawer.Screen name="Casas de acolhimento" component={Screens.Casa_acolhimento} />
+        <Drawer.Screen name="Direitos LGBTQIA+" component={Screens.Direitos} />
+        <Drawer.Screen name="Ajuda psicológica" component={Screens.Ajuda_psicologica} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
---
+++ screens/Index.js
@@ -0,0 +1,8 @@
+export { default as Home } from "./home/Home.js";
+export { default as Ajuda_psicologica } from "./ajuda_psicologica/Ajuda_psicologica.js";
+export { default as Ajude } from "./ajude/Ajude.js";
+export { default as Casa_acolhimento } from "./casa_acolhimento/Casa_acolhimento.js";
+export { default as Defesa_pessoal } from "./defesa_pessoal/Defesa_pessoal.js";
+export { default as Direitos } from "./direitos/Direitos.js";
+export { default as Localizacoes} from "./localizacoes/Localizacoes.js";
+export { default as Nome_social } from "./nome_social/Nome_social.js";
